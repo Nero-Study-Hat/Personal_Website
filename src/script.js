d9a037a0a65037bddcf056565b5de66e7e089b14
Nero-Study-Hat <85171551+Nero-Study-Hat@users.noreply.github.com>
2025-04-05 13:37:20 -0400

    Content stacking setup.

--- src/script.js
+++ src/script.js
@@ -3,9 +3,11 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+    alpha: false, /* leave background black */
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
