1503fa29ee087887996671593370f736488072cf
Nero-Study-Hat <85171551+Nero-Study-Hat@users.noreply.github.com>
2026-01-25 11:07:05 -0500

    reorganize terraform directories and code

--- terraform/dev-aws/variables.tf
+++
@@ -1,4 +0,0 @@
-# AWS region where resources will be created.
-variable "region" {
-  default = "us-east-1" # Default AWS region
-}
--- terraform/dev-aws/vpc/variables.tf
+++
@@ -1,4 +0,0 @@
-# AWS availability zone within the region.
-variable "zone" {
-  default = "us-east-1a" # Default availability zone
-}
